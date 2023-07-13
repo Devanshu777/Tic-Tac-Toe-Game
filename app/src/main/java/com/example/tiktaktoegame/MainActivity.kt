d0cf1f9b79155606840d182b8bb4f10dd97d9a54
Devanshu777 <tiwari.devanshu097@gmail.com>
2023-07-14 00:02:02 +0530

    Last Commit

--- app/src/main/java/com/example/tiktaktoegame/MainActivity.kt
+++ app/src/main/java/com/example/tiktaktoegame/MainActivity.kt
@@ -11,6 +11,9 @@ class MainActivity : AppCompatActivity() {
 
     var gameActive = true
 
+
+
+
     // Player representation
     // 0 - X
     // 1 - O
@@ -97,7 +100,7 @@ class MainActivity : AppCompatActivity() {
         return !gameState.contains(2)
     }
 
-    fun gameReset(view:  View?) {
+    fun gameReset(view: View?) {
         gameActive = true
         activePlayer = 0
         gameState = intArrayOf(2, 2, 2, 2, 2, 2, 2, 2, 2)
